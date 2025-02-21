1c710c44cb96911d2ac29b288c7981b7641e0d4a
RohanGauda <121014055+RohanGauda@users.noreply.github.com>
2025-02-21 15:34:11 +0530

    New Added

---
+++ SeleniumWithJenkins/src/test/java/pac2/OrganizationTest.java
@@ -0,0 +1,17 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	
+	@Test
+	public void createOrganizationTest() {
+		System.out.println("execute createOrganizationTest");
+	}
+	
+	@Test
+	public void modifyOrganizationTest() {
+		System.out.println("execute modifyOrganizationTest");
+	}
+
+}
